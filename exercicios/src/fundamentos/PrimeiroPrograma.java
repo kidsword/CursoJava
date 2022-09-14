aa86b7d0d83d45c21aba7586b6037eeb74baabd3
kidsword <kidsword@gmail.com>
2022-09-14 12:43:37 -0300

    first commit

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package fundamentos;
+/**
+ * JAVADOC NA CLASSE TAMBÉM
+ * @author daniel
+ *
+ */
+public class PrimeiroPrograma {
+	/**
+	 * javadoc string - gera um xml comn a documentação do código
+	 * @author daniel
+	 * @since 
+	 * @see	  
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Vai Curíntia!!!");
+		//Ctrl+Shift+F -> Acerta identação
+		
+		//Package (Pacote) = NameSpace
+		/**
+		 * Comentário de mais de uma linha
+		 * Ctrl+/ comenta código
+		 * 
+		 * */
+
+	}
+}
